7639efe0d87aa13982a7e1925d1017ecfb501811
Lam Ching Rou <crlam.2020@smu.edu.sg>
2023-09-04 18:35:53 +0800

    change from local to prod deployment v2

--- POC/main.py
+++ POC/main.py
@@ -20,5 +20,6 @@ def sns_webhook():
         # Handle any exceptions
         return jsonify({'error': str(e)}), 500
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
+    app.run()
 #     app.run(host='0.0.0.0', port=5000)
