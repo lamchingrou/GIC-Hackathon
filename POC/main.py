df898b81cf2e9bd6095261e68b64f3789ad927fb
Lam Ching Rou <crlam.2020@smu.edu.sg>
2023-09-04 17:50:29 +0800

    install requirements and receive msg from AWS SNS

---
+++ POC/main.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/sns-webhook', methods=['POST'])
+def sns_webhook():
+    try:
+        # Get the SNS message from the request
+        sns_message = request.json
+
+        # Handle the SNS message as needed
+        # In this example, we just print it
+        print("Received SNS message:")
+        print(sns_message)
+
+        # Return a success response
+        return jsonify({'message': 'SNS message received successfully'}), 200
+
+    except Exception as e:
+        # Handle any exceptions
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
